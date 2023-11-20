d32829c0f23cd4fdf0ddbc8b5d7fd20b1593245c
Mary <cameron105@mail.ru>
2023-11-21 00:39:23 +0300

    команды browser + conftest

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,12 +1,11 @@
-from selene import browser
+from selene import browser, have, be
 
 
 def test_complete_todo():
-
-    browser.config.base_url = 'https://todomvc.com/examples/emberjs'
     browser.open('/')
+    browser.element('#new-todo').should(be.blank)
 
     browser.element('#new-todo').type('a').press_enter()
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
-    browser.all(selector)
+    browser.all('#todo-list>li').should(have.size(3))
