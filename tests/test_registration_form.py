cdd9064e72d9a75cc4151a21cc9b1b5152aeff03
Mary <cameron105@mail.ru>
2023-11-25 14:48:04 +0300

    check every elements

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,6 +1,6 @@
 import os
 
-from selene import browser, have, be, by
+from selene import browser, have, be, by, command
 
 
 def test_complete_todo():
@@ -10,26 +10,32 @@ def test_complete_todo():
     browser.element('#firstName').should(be.blank).type('Mariya')
     browser.element('#lastName').should(be.blank).type('Mokretsova')
     browser.element('#userEmail').should(be.blank).type('cameron105@mail.ru')
-    browser.element('#gender-radio-1').double_click()
+    browser.element('#gender-radio-2').double_click()
     browser.element('#userNumber').should(be.blank).type('9066507373')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click().type('June').press_enter()
     browser.element('.react-datepicker__year-select').click().type('1989').press_enter()
     browser.element('.react-datepicker__day--027').click()
     browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').double_click()
+    browser.element('[for=hobbies-checkbox-3]').perform(command.js.scroll_into_view).click()
     browser.element('#uploadPicture').send_keys(os.path.abspath(
         'pictures/run_girl.png'))
-    browser.element('#currentAddress').should(be.blank).type('Sant-Peterburg')
+    browser.element('#currentAddress').should(be.blank).type('Sant-Peterburg, Aleksandra Matrosova')
 
     browser.element('#react-select-3-input').type('Rajasthan').press_enter()
     browser.element('#react-select-4-input').type('Jaiselmer').press_enter()
     browser.element('#submit').press_enter()
 
+    browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
+    browser.all('.table').all('td')[1].should(have.exact_text('Mariya Mokretsova'))
+    browser.all('.table').all('td')[3].should(have.exact_text('cameron105@mail.ru'))
+    browser.all('.table').all('td')[5].should(have.exact_text('Female'))
+    browser.all('.table').all('td')[7].should(have.exact_text('9066507373'))
+    browser.all('.table').all('td')[9].should(have.exact_text('27 June,1989'))
+    browser.all('.table').all('td')[11].should(have.exact_text('English'))
+    browser.all('.table').all('td')[13].should(have.exact_text('Music'))
+    browser.all('.table').all('td')[15].should(have.exact_text('run_girl.png'))
+    browser.all('.table').all('td')[17].should(have.exact_text('Sant-Peterburg, Aleksandra Matrosova'))
+    browser.all('.table').all('td')[19].should(have.exact_text('Rajasthan Jaiselmer'))
 
-    browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
-    browser.element('.table-responsive').should(have.text(
-        'Mariya Mokretsova' and 'cameron105@mail.ru'
-        and 'Male' and '9066507373' and '27 June,1989'
-        and 'English' and 'run_girl.png'
-        and 'Sant-Peterburg' and 'Rajasthan Jaiselmer'))
+    browser.element('#closeLargeModal').press_enter()
